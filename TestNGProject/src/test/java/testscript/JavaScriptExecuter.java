03f4c5b98ebbdc3b008444dca0bb3c90752900f8
ranji <letsmailjith@gmail.com>
2025-06-08 10:54:07 +0530

    Code Transferred

--- TestNGProject/src/test/java/testscript/JavaScriptExecuter.java
+++ TestNGProject/src/test/java/testscript/JavaScriptExecuter.java
@@ -12,10 +12,12 @@ public class JavaScriptExecuter extends Base
 	{
 		driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
 		JavascriptExecutor js = (JavascriptExecutor) driver;
+		
 		WebElement entermsg=driver.findElement(By.xpath("//input[@id='single-input-field']"));
-		js.executeScript("arguments[0].value='Hello';", entermsg);
+		js.executeScript("arguments[0].value='Hello';", entermsg); //sendkeys()
+		
 		WebElement btn=driver.findElement(By.xpath("//button[@id='button-one']"));
-		js.executeScript("arguments[0].click();", btn);
+		js.executeScript("arguments[0].click();", btn); //click()
 		
 	}
 	@Test
@@ -25,7 +27,7 @@ public class JavaScriptExecuter extends Base
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,150)","");
 		
-		//https://groceryapp.uniqassosiates.com/admin
+		js.executeScript("window.scrollBy(0,document.body.scrollHeight)","");
 	}
 
 }
